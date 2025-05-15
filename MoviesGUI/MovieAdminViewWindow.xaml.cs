bf4b6eadd974055631724137b7e13b2fc272594c
Menna <manameno2005yahoo.com@gmail.com>
2025-05-15 16:07:18 +0300

    Admin movie page

---
+++ MoviesGUI/MovieAdminViewWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoviesGUI
+{
+    /// <summary>
+    /// Interaction logic for MovieUserWindow.xaml
+    /// </summary>
+    public partial class MovieAdminViewWindow : Window
+    {
+        public MovieAdminViewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Edit button clicked!");
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Delete button clicked!");
+        }
+
+    }
+}
