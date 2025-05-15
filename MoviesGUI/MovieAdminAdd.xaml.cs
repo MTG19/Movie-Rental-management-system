eb377f3bc5f587c9e334d5be7b65a4c4b33c375d
Menna <manameno2005yahoo.com@gmail.com>
2025-05-15 14:10:28 +0300

    Admin add movie page

---
+++ MoviesGUI/MovieAdminAdd.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoviesGUI
+{
+    /// <summary>
+    /// Interaction logic for MovieAdminAdd.xaml
+    /// </summary>
+    public partial class AddMovieAdminWindow : Window
+    {
+        public AddMovieAdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Add event handlers and logic here
+    }
+}
