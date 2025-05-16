a5eba9d4e1161de96f77beeefee402d1e0f0bb9f
MirnaElHefny <11410120231185@stud.cu.edu.eg>
2025-05-16 20:59:47 +0300

    merge

--- MoviesGUI/obj/Debug/net8.0-windows/RentingDetails.g.i.cs
+++ MoviesGUI/obj/Debug/net8.0-windows/RentingDetails.g.i.cs
@@ -77,7 +77,7 @@ namespace MoviesGUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MoviesGUI;V1.0.0.0;component/rentingdetails.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MoviesGUI;component/rentingdetails.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\RentingDetails.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
