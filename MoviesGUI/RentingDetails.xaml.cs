d4b0fd74c5b1e074552ed5d84b5ed974f1466379
MirnaElHefny <11410120231185@stud.cu.edu.eg>
2025-05-18 01:25:23 +0300

    added the rentingDetails back

--- MoviesGUI/RentingDetails.xaml.cs
+++ MoviesGUI/RentingDetails.xaml.cs
@@ -1,33 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MoviesGUI
 {
-    /// <summary>
-    /// Interaction logic for RentingDetails.xaml
-    /// </summary>
     public partial class RentingDetails : Window
     {
-        public RentingDetails(int movieId)
+        public ObservableCollection<RentingOrderItem> RentOrders { get; set; }
 
+        public RentingDetails(int movieId)
         {
             InitializeComponent();
+            RentOrders = new ObservableCollection<RentingOrderItem>();
+            RentingOrderGrid.ItemsSource = RentOrders;
+            UpdateTotals();
         }
 
-        private void MovieTitleBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void AddMovie_Click(object sender, RoutedEventArgs e)
         {
+            DateTime rentDate = RentDatePicker.SelectedDate ?? DateTime.Now;
+            DateTime returnDate = rentDate.AddMonths(1);
 
+            var newItem = new RentingOrderItem
+            {
+                MovieTitle = $"Movie {RentOrders.Count + 1}",
+                Amount = 1,
+                RentDate = rentDate,
+                ReturnDate = returnDate,
+                Price = 5.00 * 1
+            };
+
+            RentOrders.Add(newItem);
+            UpdateTotals();
         }
+
+        private void CancelAll_Click(object sender, RoutedEventArgs e)
+        {
+            RentOrders.Clear();
+            UpdateTotals();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var item = button?.DataContext as RentingOrderItem;
+
+            if (item != null && RentOrders.Contains(item))
+            {
+                RentOrders.Remove(item);
+                UpdateTotals();
+            }
+        }
+
+        private void UpdateTotals()
+        {
+            TotalMoviesText.Text = RentOrders.Count.ToString();
+            double totalPrice = RentOrders.Sum(item => item.Price);
+            TotalPriceText.Text = $"${totalPrice:0.00}";
+        }
+
+        private void PlaceOrder_Click(object sender, RoutedEventArgs e)
+        {
+            if (RentOrders.Count == 0)
+            {
+                MessageBox.Show("No movies in the renting order.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            string paymentMethod = CashRadio.IsChecked == true ? "Cash" :
+                                   CardRadio.IsChecked == true ? "Credit Card" :
+                                   "Online Payment";
+
+            MessageBox.Show($"Order placed successfully!\nPayment Method: {paymentMethod}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            RentOrders.Clear();
+            UpdateTotals();
+        }
+    }
+
+    public class RentingOrderItem
+    {
+        public string MovieTitle { get; set; }
+        public int Amount { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public double Price { get; set; }
     }
 }
--- MoviesGUI/obj/Debug/net8.0-windows/MoviesGUI.AssemblyInfo.cs
+++ MoviesGUI/obj/Debug/net8.0-windows/MoviesGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c79d9b16c7f1e87d19605d491d67ac3fafdf9cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907ee613d4101500ceb2c51a50b2002af70eba90")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
