a1b30e7433f39f07fc8a0d0e6e641f3def114e54
Menna <manameno2005yahoo.com@gmail.com>
2025-05-18 21:23:09 +0300

    Rental Report

---
+++ MoviesGUI/RentalReport.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+
+namespace MoviesGUI
+{
+    public partial class RentalReport : Window
+    {
+        string connectionString = @"Server=localhost;Database=MovieRental;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public RentalReport()
+        {
+            InitializeComponent();
+            LoadRentalReport();
+        }
+
+        private void LoadRentalReport()
+        {
+            string query = @"
+                SELECT 
+                    u.Name AS UserName,
+                    m.Title AS MovieTitle,
+                    ro.rentingDate,
+                    ro.returnDate,
+                    rd.rentingPrice,
+                    p.Method AS PaymentMethod,
+                    p.PaymentDate
+                FROM Users u
+                JOIN rentingOrder ro ON u.UserID = ro.UserID
+                JOIN rentingDetail rd ON ro.RentalID = rd.RentalID
+                JOIN Library l ON rd.TapeID = l.TapeID
+                JOIN Movie m ON l.MovieID = m.MovieID
+                LEFT JOIN payment p ON ro.RentalID = p.RentalId
+                ORDER BY u.Name, ro.rentingDate;";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dgRentalReport.ItemsSource = dt.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading report: " + ex.Message);
+            }
+        }
+    }
+}
+
--- MoviesGUI/obj/Debug/net8.0-windows/MoviesGUI.AssemblyInfo.cs
+++ MoviesGUI/obj/Debug/net8.0-windows/MoviesGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d953c0faa454ba890e7e78626a765018a92bd5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b05517d22f7b4eef6159af87d09c61a7c2ec88")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
