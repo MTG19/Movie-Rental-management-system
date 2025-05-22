189fc272472e1aa3ad51004091c390bc5abbae64
MirnaElHefny <11410120231185@stud.cu.edu.eg>
2025-05-22 14:19:30 +0300

    .

--- MoviesGUI/obj/Debug/net8.0-windows/MoviesGUI.AssemblyInfo.cs
+++ MoviesGUI/obj/Debug/net8.0-windows/MoviesGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a300a7501e42b08555b52d4e62d5c10f3dc5c45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9aeb5e97a023a7a93e4754b46fe98a1774e364")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
